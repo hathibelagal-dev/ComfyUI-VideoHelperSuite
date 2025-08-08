517d737f3c01fafd850c6a3752f78a2686e83826
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-08-08 12:18:40 +0530

    fix to remove server

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from .videohelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 import folder_paths
 from .videohelpersuite import documentation
-from .videohelpersuite import latent_preview
 
 WEB_DIRECTORY = "./web"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
