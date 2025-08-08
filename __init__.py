5c85d11feefbed5ab4d4f01bcd017bf25b5d6ada
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-08-08 12:17:48 +0530

    fix to remove server

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from .videohelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 import folder_paths
-from .videohelpersuite.server import server
 from .videohelpersuite import documentation
 from .videohelpersuite import latent_preview
 
